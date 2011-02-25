dfaba85d442e04a4f14bcd7eba5d4755bfaf09de
thomas <thomas@fourier>
2011-02-26 07:04:58 +1100

    Updated about box version number.

--- src/org/ingatan/component/statcentre/RewardItem.java
+++ src/org/ingatan/component/statcentre/RewardItem.java
@@ -7,7 +7,6 @@ package org.ingatan.component.statcentre;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
